519bfc509d8f30932f2a90ed703a9f5cfa1e7550
Dane Finch <Dane@Danes-MacBook.local>
2018-11-04 09:27:57 -0800

    changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LIRI-Bot
-LIRI Bot
+LIRI Bot<br>
+Overview<br>
+Included are the files that will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and<br> Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app<br>
+that takes in parameters and gives you back data.
+
--- liri.js
+++ liri.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
+var request = require("request");
+var keys = require("./keys.js");
 var inquirer = require('inquirer');
-var request = require('request');
+var Spotify = require("node-spotify-api");
+var moment = require('moment');
+var fs = require("fs");
 
 var searchTerm = "";
 
@@ -14,26 +16,28 @@ function prompt() {
                 type: "list",
                 name: "command",
                 message: "What would you like to do?",
-                choices: ["Get song", "Get movie"],
+                choices: ["Get song", "Get movie", "Get concert", "Do what it says"],
             },
-            // {
-            //     type: "input",
-            //     name: "searchterm",
-            //     message: "What would you like to search for",
-            // }
-            /* Pass your questions in here */
         ])
         .then(function (answers) {
             console.log(answers);
-            if (answers.command==="Get song") {
-                console.log("runningfunction")
-            }
-            if (answers.command==="Get movie"){
+            if (answers.command === "Get song") {
                 search(answers.command)
-                //console.log("newfunction")
-                //getMovie(searchTerm)
+                //console.log("function")
             }
-            // Use user feedback for... whatever!!
+            if (answers.command === "Get movie") {
+                search(answers.command)
+                //console.log("newFunction")
+            }
+            if (answers.command === "Get concert") {
+                search(answers.command)
+                //console.log("newFunction2")
+            }
+            if (answers.command === "Do what it says") {
+                search(answers.command)
+                //console.log("newFunction2")
+            }
+
         });
 }
 
@@ -43,32 +47,99 @@ function search(command) {
             {
                 type: "input",
                 name: "searchterm",
-                message: "What would you like to search for",
+                message: "What would you like to search for?",
             }
-            /* Pass your questions in here */
         ])
         .then(function (answers) {
-          searchTerm = answers.searchterm;
-            if (command === "Get movie"){
+            searchTerm = answers.searchterm;
+            if (command === "Get movie") {
                 getMovie(searchTerm)
             }
-            // Use user feedback for... whatever!!
+
+            if (command === "Get song") {
+                getSong(searchTerm)
+            }
+            if (command === "Get concert") {
+                getConcert(searchTerm)
+            }
+            if (command === "Do what it says") {
+                doWhatItSays(searchTerm)
+            }
+
         });
 }
 
-
 function getMovie(movie) {
- console.log(movie)
- if (movie==="") {
-     console.log("no search term")
-     movie="Mr. Nobody"
- }
- var queryurl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=full&tomatoes=true&apikey=trilogy";
- request(queryurl, function(error, response, body){
-     if (!error && response.statusCode===200){
-         console.log(body)
-     }
- })
+    console.log(movie)
+    if (movie === "") {
+        console.log("no search term")
+        movie = "Mr. Nobody"
+    }
+    var queryurl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+    request(queryurl, function (error, response, body) {
+       
+        if (!error && response.statusCode === 200) {
+            //console.log(body)
 
-} 
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    })
+}
+
+function getSong(song) {
+    var spotify = new Spotify(keys.spotify);
+    if (searchTerm === "") {
+        console.log("no search term")
+        song = 'Ace of Base: The Sign';
+    }
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        var songInfo = data.tracks.items;
+            console.log("Artist(s): " + songInfo[0].artists[0].name);
+            console.log("Song Name: " + songInfo[0].name);
+            console.log("Preview Link: " + songInfo[0].preview_url);
+            console.log("Album: " + songInfo[0].album.name);
+        
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+    });
+}
+
+function getConcert(artist) {
+    console.log(artist)
+    if (artist === "") {
+        console.log("no search term")
+        artist = "Tycho"
+    }
+    var queryurl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    request(queryurl, function (error, response, body) {
+            console.log("Venue Name: " + JSON.parse(body)[0].venue.name);
+            console.log("Venue Location: " + JSON.parse(body)[0].venue.city);
+            console.log("Event Date: " +  moment(JSON.parse(body)[0]).format("MM/DD/YYYY"));
+        if (!error && response.statusCode === 200) {
+            //console.log(body)
+        }
+    })
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+          return console.log(error);
+        }
+         //console.log(data);
+         var dataArr = data.split(",");
+         console.log(dataArr);
+         
+         prompt();
+      });
+}
+
 prompt();
