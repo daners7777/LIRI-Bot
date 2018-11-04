91ac2f529a741ae6a4c7aa9ac7b0cf3939a75e98
Dane Finch <Dane@Danes-MacBook.local>
2018-11-03 17:15:38 -0700

    ignore

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
-// require("dotenv").config();
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
 
 var inquirer = require('inquirer');
 var request = require('request');
